683fe3d17e7eef0e6fa08060eb5a1318be45d202
github_camilooroja <camilo.1948@hotmail.com>
2022-09-18 10:40:54 -0500

    endpoints parkink correct

--- backend/src/routes/parking.js
+++ backend/src/routes/parking.js
@@ -2,4 +2,16 @@ const { Router } = require('express');
 const router = Router();
 
 // importa metodo
-const { getStudents, createStudent, getStudent, updateStudent, deleteStudent } = require('../controllers/students.controller');
+const { getAllParking, createParking, getParking, updateParking, deleteParking } = require('../controllers/parking.controller');
+
+router.route('/')
+    .get(getAllParking)
+    .post(createParking)
+
+router.route('/:id')
+    //.get((req, res) => res.send('ACA SI IMPRIMO '+ typeof(req.params.id)))   
+    .get(getParking)
+    .put(updateParking)
+    .delete(deleteParking)
+
+module.exports = router;
