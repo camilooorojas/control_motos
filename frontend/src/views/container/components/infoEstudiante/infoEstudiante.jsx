8193bfa8c2a0479f1f05d4c4138ba15d456fda46
Daniel <danilog1000@gmail.com>
2022-09-18 01:30:56 -0500

    infoEstudiante completed

--- frontend/src/views/container/components/infoEstudiante/infoEstudiante.jsx
+++ frontend/src/views/container/components/infoEstudiante/infoEstudiante.jsx
@@ -1,9 +1,43 @@
 import styles from './infoEstudianteStyles.module.css';
 
+import { CardEstudiante } from './components/CardStudent/CardEstudiante';
+
 function InfoEstudiante() {
+  const data = [
+    {
+      plate: "aaa",
+      propertyId: 123
+    },
+    {
+      plate: "ccc",
+      propertyId: 456
+    }
+  ]
+
   return (
     <div className={styles.container}>
       <h2>Datos del estudiante</h2>
+      <div className={styles.bg_white}>
+        <div className={styles.student_data}>
+          <div>Nombre: </div>
+          <div>Apellido: </div>
+          <div>Cédula: </div>
+          <div>Código: </div>
+        </div>
+        <div className={styles.card_content}>
+          {data?.map(item => (
+            <CardEstudiante 
+              propertyId={item.plate}
+              plate={item.propertyId}
+            />
+          ))}
+        </div>
+        <div className={styles.button_container}>
+          <button>
+            Registrar nueva moto
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/views/container/container.jsx
+++ frontend/src/views/container/container.jsx
@@ -6,7 +6,7 @@ import { RegistroSalida } from './components/registroSalida/registroSalida';
 import { RegistroEstudiante } from './components/registroEstudiante/registroEstudiante';
 import { Historial } from './components/history/historial';
 import { MotosIngresadas } from './components/motosIngresadas/motosIngresadas';
-
+import { InfoEstudiante } from './components/infoEstudiante/infoEstudiante';
 
 function Container() {
   
@@ -15,7 +15,6 @@ function Container() {
       <BrowserRouter>
         <NavBar />
         <h1>PARQUEADERO UD</h1>
-
         <Routes>
           <Route path="/" element={<RegistroIngreso />} />
           <Route path="/ingreso" element={<RegistroIngreso />} />
@@ -23,6 +22,7 @@ function Container() {
           <Route path="/registro" element={<RegistroEstudiante/>}/>
           <Route path="/historial" element={<Historial/>}/>
           <Route path="/motos" element={<MotosIngresadas/>}/>
+          <Route path="/info-student" element={<InfoEstudiante/>}/>
         </Routes>
       </BrowserRouter>
     </div>
