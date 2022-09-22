96329445bb82605b3ba0d94a95b7b0ce4af9a7fd
camilooorojas <camilo.1948@hotmail.com>
2022-09-21 22:33:24 -0500

    commit

--- frontend/src/views/container/components/registroIngreso/registroIngreso.jsx
+++ frontend/src/views/container/components/registroIngreso/registroIngreso.jsx
@@ -5,7 +5,7 @@ import { RegistroMoto } from '../registroMoto/registroMoto';
 import styles from './registroIngresoStyles.module.css';
 
 function RegistroIngreso(props) {
-  
+
   const [students, setStudents] = useState([]);
   const [search, setSearch] = useState("");
   const [bikes, setBikes] = useState([]);
@@ -18,6 +18,7 @@ function RegistroIngreso(props) {
     }
   }
   const callApi = async (code) => {
+    setNewMoto(false);
     setBikes([]);
     setStudents([]);
     console.log(students);
@@ -25,17 +26,17 @@ function RegistroIngreso(props) {
       const res = await axios.get(`http://localhost:4000/api/students/${code}`);
       setStudents(res.data);
     }
-    
+
   }
 
   return (
     <div className={"d-flex flex-column justify-content-center align-items-center"}>
       <h2>Registro Ingreso</h2>
       <input autoFocus type="number" value={search} onKeyDown={handleClick} onChange={handleSearch} className={styles.inputName} placeholder="Digite código del estudiante..." />
-      <button className={styles.findButton} onClick={() => { callApi(search)}}>Buscar</button>
-      <div className={styles.infoData}>{students.length > 0 ? (<> {newMoto === false ? <InfoEstudiante student={students[0]} bikes={bikes} setBikes={setBikes} setNewMoto={setNewMoto}/> : <RegistroMoto/>} </>) : <h2>El estudiante no está registrado</h2>}</div>
+      <button className={styles.findButton} onClick={() => { callApi(search) }}>Buscar</button>
+      <div className={styles.infoData}>{students.length > 0 ? <>  <InfoEstudiante student={students[0]} bikes={bikes} setBikes={setBikes} setNewMoto={setNewMoto} />  </> : <h2>El estudiante no está registrado</h2>}</div>
     </div>
   );
 }
 
-export {RegistroIngreso}
+export { RegistroIngreso }
